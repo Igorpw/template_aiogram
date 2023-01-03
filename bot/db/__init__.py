8fc150593e47e1e623d056d0ef1fd12167a20dd7
FAST-CODE-GITHUB <fast.code.auth@gmail.com>
2023-01-03 17:49:24 +0300

    Обновил файл с модулями

--- bot/db/__init__.py
+++ bot/db/__init__.py
@@ -1,4 +1,4 @@
 from .models import User
-from .base import Base
+from .base import Base, metadata
 
-__all__ = ['User', 'Base']
+__all__ = ['User', 'Base', 'metadata']
--- bot/db/base.py
+++ bot/db/base.py
@@ -2,3 +2,4 @@ from sqlalchemy.orm import declarative_base
 
 # Декларативная база
 Base = declarative_base()
+metadata = Base.metadata
